d504323fad5a4f08cc7454835e2101bf05094853
Luthfiyya Ayu <luthfiyyaaayu@gmail.com>
2024-09-27 22:55:02 +0700

    second commit

--- mobile.js
+++ mobile.js
@@ -14,3 +14,18 @@ setInterval(() => {
         nameElement.style.opacity = 1;
     }, 1000); // Waktu sesuai durasi fade-out (1 detik)
 }, 3000); // Ganti teks setiap 3 detik
+
+const hamburger = document.querySelector(".hamburger");
+        const navMenu = document.querySelector(".nav-menu");
+
+        hamburger.addEventListener("click", () => {
+            hamburger.classList.toggle("active");
+            navMenu.classList.toggle("active");
+        });
+
+        document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", () => {
+            hamburger.classList.remove("active");
+            navMenu.classList.remove("active");
+        }));
+
+        
